c2fe00089ee7a8692bf085fb86e13635f5289c6c
ErickJulio <erickwiejulio@gmail.com>
2023-11-09 20:24:03 -0300

    ajuste

--- app.js
+++ app.js
@@ -156,11 +156,22 @@ app.post('/inserir-dados', async (req, res) => {
       return res.status(400).json({ mensagem: 'Campos obrigatórios estão faltando' });
     }
 
+    // Verifica se o login já existe na tabela
+    const checkLoginQuery = 'SELECT COUNT(*) FROM cadastro_user WHERE login = $1';
+    const loginCount = await db.one(checkLoginQuery, [login], (data) => +data.count);
+
+    if (loginCount > 0) {
+      return res.status(400).json({ mensagem: 'Login já existe. Escolha outro login.' });
+    }
+
     // Hash da senha antes de inserir no banco de dados
     const hashedSenha = await bcrypt.hash(senha, 10);
 
     // Executa a consulta SQL para inserção de dados
-    const insertQuery = `INSERT INTO cadastro_user (login, senha, ddd, celular, rua, numero, cep, cidade, estado) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`;
+    const insertQuery = `
+      INSERT INTO cadastro_user (login, senha, ddd, celular, rua, numero, cep, cidade, estado)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+    `;
     await db.none(insertQuery, [login, hashedSenha, ddd, celular, rua, numero, cep, cidade, estado]);
 
     res.status(200).json({ mensagem: 'Dados inseridos com sucesso' });
@@ -169,6 +180,7 @@ app.post('/inserir-dados', async (req, res) => {
     res.status(500).json({ erro: 'Erro ao inserir dados' });
   }
 });
+
 
 app.post('/esqueci-senha', async (req, res) => {
   const { login, senha, confirmarSenha } = req.body;
